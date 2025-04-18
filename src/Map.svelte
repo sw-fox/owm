7228cd3b6a3af8fc381271a810bf9c43e23f8314
swfox <swfox@gmx.de>
2025-04-18 14:49:03 +0200

    fix position and add types

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     import L from "leaflet";
     import "leaflet/dist/leaflet.css";  //needs to be imported for proper rendering!
-    import "leaflet.locatecontrol"; // Import plugin
+    import "leaflet.locatecontrol";
     import "leaflet.locatecontrol/dist/L.Control.Locate.min.css"; // Import styles
+    import { LocateControl } from "leaflet.locatecontrol";
 
     export var lat=48.783;
 	export var lon=9.183;
 	export var zoom=13;
-    export function changeView(new_lat,new_lon,new_zoom){
-		console.log("function not injected");
-	};
 
     const marker_image = L.icon({
         iconUrl: 'images/marker-icon.png',
@@ -22,14 +20,23 @@
         popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
     });
 
-    function createMap(container){
+    function createMap(container: HTMLDivElement){
         var map = L.map(container).setView([lat, lon], zoom);
-        //map.locate({setView: true, maxZoom: 16}); //manual go to user location
+        map.locate({setView: true, maxZoom: 16}); //manual go to user location
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
-        L.control.locate().addTo(map);
+
+        let lc = new LocateControl({
+            flyTo: true,
+            keepCurrentZoomLevel: true,
+            showCompass: true, // optional
+            strings: {
+                title: "Standort anzeigen"
+            },
+            }).addTo(map);
+
         map.on('dragend', function onDragEnd(){
             fetchOverpass(map);
         });
@@ -60,6 +67,10 @@
                 'nwr["fountain"="bottle_refill"]('+bbox+');' +
                 'nwr["natural"="spring"]('+bbox+');' +
                 'nwr["man_made"="water_well"]('+bbox+');' +
+                'way["landuse"="cemetery"]('+bbox+');' +
+                'relation["landuse"="cemetery"]('+bbox+');' +
+                'way["amenity"="grave_yard"]('+bbox+');' +
+                'relation["amenity"="grave_yard"]('+bbox+');' +
                 ');+out+geom;';
             const response = await fetch('https://overpass-api.de/api/interpreter', {
                 method: 'POST',
@@ -68,6 +79,11 @@
             });
 
             response.json().then(parsedResponse => {
+                map.eachLayer((layer) => {  //remove all previous markers for performance reasons
+                    if (layer instanceof L.Marker) {
+                        layer.remove();
+                    }
+                });
                 parsedResponse.elements.forEach(element => {
                     if(element.tags.drinking_water == "no"){
                         return; //skip if no drinking water
@@ -89,29 +105,29 @@ function renderPopup(element){
     renderPopupLine("amenity", element.tags.amenity) +
     renderPopupLine("drinking_water", element.tags.drinking_water) +
     renderPopupLine("fountain", element.tags.fountain) +
-    renderPopupLine("tourism", element.tags.tourism	) +
-    renderPopupLine("man_made", element.tags.man_made	) +
-    renderPopupLine("natural", element.tags.natural	) +
+    renderPopupLine("tourism", element.tags.tourism) +
+    renderPopupLine("man_made", element.tags.man_made) +
+    renderPopupLine("natural", element.tags.natural) +
     renderPopupLine("description", element.tags.description) +
     renderWikidataLink( element.tags.wikidata) +
     renderLink( element.tags.website);
 }
 
-function renderPopupLine(label, value){
+function renderPopupLine(label:string, value:string){
     if(value){
         return label + ": " + value + "<br/>"
     }
     return "";
 }
 
-function renderWikidataLink( value){
+function renderWikidataLink( value:string){
     if(value){
         return "wikidata: <a href='https://www.wikidata.org/wiki/" + value + "' target='_blanc'>Link</a><br/>"
     }
     return "";
 }
 
-function renderLink( value){
+function renderLink( value: string ){
     if(value){
         return "website: <a href='" + value + "' target='_blanc'>Link</a><br/>"
     }
@@ -121,4 +137,12 @@ function renderLink( value){
 </script>
 
 
-<div class="map" style="height:100%;width:100%;overflow:hidden" use:createMap />
+<div class="map" use:createMap ></div>
+
+<style>
+.map{
+    height: 100vh;
+    width: 100%;
+    overflow: hidden;
+}
+</style>
